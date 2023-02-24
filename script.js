672038fa9d7fdbce628b2fe73e9e4060cc7cd919
Anup Angadi <52240005+Anup9148680234@users.noreply.github.com>
2023-02-24 16:53:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
+const userInput = document.getElementById("newTodoInput");
+const btn = document.getElementById("addTodoBtn");
+const list = document.getElementById("todoList");
 
+btn.addEventListener("click" , function() {
+	const Inputtrimed = userInput.value.trim();
+	if(Inputtrimed != ""){
+		const li = document.createElement("li");
+		li.textContent = Inputtrimed;
+		list.appendChild(li);
+		userInput.value = "";
+	}
+})
